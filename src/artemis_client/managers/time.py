a5da61dd88f427f9ec9d17135be206f07501c75c
Lucas Alber <lucasd.alber@gmail.com>
2021-12-08 09:57:18 +0100

    time: Added docstring

--- src/artemis_client/managers/time.py
+++ src/artemis_client/managers/time.py
@@ -3,6 +3,8 @@ from .manager import ArtemisManager
 
 
 class TimeManager(ArtemisManager):
+    """ This manager queries the /time endpoint of Artemis.
+    """
     async def time(self):
         resp = await self._session.get_endpoint("/time")
         text: str = await resp.text()
