39fede4461fac485d9397e92bb21cbadf78e2ec6
Lucas Alber <lucasd.alber@gmail.com>
2021-12-08 12:20:56 +0100

    Fix user tests

--- tests/api/test_user.py
+++ tests/api/test_user.py
@@ -11,7 +11,7 @@ from artemis_client.session import ArtemisSession
 
 @pytest.mark.asyncio
 async def test_get_users(artemis_session: ArtemisSession):
-    found = True
+    found = False
     async for user in artemis_session.user.get_users():
         check_type("user", user, UserDTO)
         if user["login"] == artemis_session.get_username():
@@ -21,9 +21,17 @@ async def test_get_users(artemis_session: ArtemisSession):
 
 @pytest.mark.asyncio
 async def test_search_users(artemis_session: ArtemisSession):
-    found = True
+    found = False
     async for user in artemis_session.user.search_users(artemis_session.get_username()):
         check_type("user", user, SearchUserDTO)
         if user["login"] == artemis_session.get_username():
             found = True
     assert found
+
+
+@pytest.mark.asyncio
+async def test_search_users_missing(artemis_session: ArtemisSession):
+    found = False
+    async for _ in artemis_session.user.search_users("someRandomStringThatIsNotAUser"):
+        found = True
+    assert not found
