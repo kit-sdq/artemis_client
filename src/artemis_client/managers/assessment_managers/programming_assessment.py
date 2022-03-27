5a392e0237996aab56895e704ad1b4e1a59f76fb
Lucas Alber <lucasd.alber@gmail.com>
2022-03-27 16:47:51 +0200

    chore: black

--- src/artemis_client/managers/assessment_managers/programming_assessment.py
+++ src/artemis_client/managers/assessment_managers/programming_assessment.py
@@ -1,5 +1,15 @@
+from aiohttp.client_reqrep import ClientResponse
 from artemis_client.managers.manager import ArtemisManager
+from artemis_client.api import Result
 
 
 class ProgrammingAssessmentManager(ArtemisManager):
-    pass
+    async def save_assessment(
+        self, participation_id: int, result: Result, submit: bool = False
+    ) -> ClientResponse:
+        params = {"submit": submit}
+        return await self._session.put_api_endpoint(
+            f"/participations/{participation_id}/manual-results",
+            json=result,
+            params=params,
+        )
--- src/artemis_client/managers/submission_managers/programming_submission.py
+++ src/artemis_client/managers/submission_managers/programming_submission.py
@@ -3,6 +3,7 @@ from artemis_client.api import Participation, ProgrammingSubmission, SubmissionT
 from artemis_client.managers import ArtemisManager
 from artemis_client.utils.serialize import loads
 
+
 class ProgrammingSubmissionManager(ArtemisManager):
     async def trigger_build(
         self, participation: Participation, submission_type: SubmissionType = "MANUAL"
@@ -21,7 +22,9 @@ class ProgrammingSubmissionManager(ArtemisManager):
             params=params,
         )
 
-    async def lock_and_get_submission(self, submission_id: int, correction_round: int = 0) -> ProgrammingSubmission:
+    async def lock_and_get_submission(
+        self, submission_id: int, correction_round: int = 0
+    ) -> ProgrammingSubmission:
         """
         Locks the submission for assessment and gets the submission.
 
@@ -31,7 +34,8 @@ class ProgrammingSubmissionManager(ArtemisManager):
         :type       correction_round:  int
         """
         params = {"correction-round": correction_round}
-        resp = await self._session.get_api_endpoint(f"/programming-submissions/{submission_id}/lock", params=params)
+        resp = await self._session.get_api_endpoint(
+            f"/programming-submissions/{submission_id}/lock", params=params
+        )
         jdict = await resp.json(loads=loads)
         return jdict
-
