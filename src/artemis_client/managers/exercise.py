01d6ee3c0b6e7c1aa69832cecf5d0e152d186524
Lucas Alber <lucasd.alber@gmail.com>
2022-10-13 12:41:06 +0200

    Add api call to exercise with template and solution

--- src/artemis_client/managers/exercise.py
+++ src/artemis_client/managers/exercise.py
@@ -1,5 +1,5 @@
 from typing import AsyncGenerator, List
-from artemis_client.api import StudentParticipation
+from artemis_client.api import ProgrammingExercise, StudentParticipation
 from artemis_client.managers.manager import ArtemisManager
 from artemis_client.utils.serialize import loads
 
@@ -14,3 +14,16 @@ class ExerciseManager(ArtemisManager):
         participations: List[StudentParticipation] = await resp.json(loads=loads)
         for participation in participations:
             yield participation
+
+    async def get_with_template_and_solution(self, exercise_id: int, with_submission_results: bool = False) -> ProgrammingExercise:
+        """
+        Returns the exercise including the template and solution participation.
+
+        :param      exercise_id:  The exercise id.
+        :type       exercise_id:  int
+        """
+        params = {
+            "withSubmissionResults": str(with_submission_results).lower()
+        }
+        resp = await self._session.get_api_endpoint(f"/programming-exercises/{exercise_id}/with-template-and-solution-participation", params=params)
+        return await resp.json(loads=loads)
