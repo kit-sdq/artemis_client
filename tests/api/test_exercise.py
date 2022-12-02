2815233efb7dacd8a16e87b1bb4600caf7ce3f71
Lucas Alber <lucasd.alber@gmail.com>
2022-12-02 11:07:20 +0100

    Fix test dependencies

--- tests/api/test_exercise.py
+++ tests/api/test_exercise.py
@@ -25,25 +25,6 @@ async def test_get_results(artemis_session: ArtemisSession):
 
 @pytest.mark.dependency()
 @pytest.mark.asyncio
-async def test_get_participations(artemis_session: ArtemisSession):
-    # If this fails a new participation type may be added.
-    async for c in artemis_session.course.get_courses_with_stats():
-        async for e in artemis_session.course.get_exercises_for_course(c["id"]):
-            async for p in artemis_session.exercise.get_participations(e["id"]):
-                check_type("Participation", p, Participation)
-                return
-    pytest.skip("Result not found")
-
-
-@pytest.mark.dependency(
-    [
-        "test_get_participations_ProgrammingExerciseStudentParticipation",
-        "test_get_participations_TemplateProgrammingExerciseParticipation",
-        "test_get_participations_TemplateProgrammingExerciseParticipation",
-        "test_get_participations_SolutionProgrammingExerciseParticipation",
-    ]
-)
-@pytest.mark.asyncio
 async def test_get_participations_StudentParticipation(artemis_session: ArtemisSession):
     async for c in artemis_session.course.get_courses_with_stats():
         async for e in artemis_session.course.get_exercises_for_course(c["id"]):
@@ -105,4 +86,23 @@ async def test_get_participations_SolutionProgrammingExerciseParticipation(
                         SolutionProgrammingExerciseParticipation,
                     )
                     return
+    pytest.skip("Result not found")
+
+
+@pytest.mark.dependency(
+    depends=[
+        "test_get_participations_ProgrammingExerciseStudentParticipation",
+        "test_get_participations_TemplateProgrammingExerciseParticipation",
+        "test_get_participations_TemplateProgrammingExerciseParticipation",
+        "test_get_participations_SolutionProgrammingExerciseParticipation",
+    ]
+)
+@pytest.mark.asyncio
+async def test_get_participations(artemis_session: ArtemisSession):
+    # If this fails a new participation type may be added.
+    async for c in artemis_session.course.get_courses_with_stats():
+        async for e in artemis_session.course.get_exercises_for_course(c["id"]):
+            async for p in artemis_session.exercise.get_participations(e["id"]):
+                check_type("Participation", p, Participation)
+                return
     pytest.skip("Result not found")
